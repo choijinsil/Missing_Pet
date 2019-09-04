d9ec7bd2b62352433900ac2e1856e49674d203a4
younyoobin <younbori1011@gmail.com>
2019-09-04 15:00:12 +0900

    1. 글 작성자는 목격신고 못하게 막기 2. 마이페이지, 게시글관리 뷰 수정

--- Missing_Pet/src/beans/missing/controller/UserController.java
+++ Missing_Pet/src/beans/missing/controller/UserController.java
@@ -146,11 +146,11 @@ public class UserController extends HttpServlet {
 			user.setBlack(request.getParameter("black"));
 
 			// 회원정보수정
-			
 			dao.update_myinfo(user);
 
 			// 회원정보,회원MISSING정보 SESSION객체 영역에 저장
 			UserVO userlist = dao.select_myinfo(loginId);
+			request.getSession().setAttribute("loginId", loginId);
 			
 			HttpSession session = request.getSession();
 			session.setAttribute("userlist", userlist);
@@ -158,7 +158,7 @@ public class UserController extends HttpServlet {
 			session.setAttribute("missinglist", missinglist);
 			
 			// 리다이렉트이동
-			response.sendRedirect("/views/user/mypage.jsp");
+			response.sendRedirect("/main?action=user_mypage");
 
 		} else if (action.equals("update_mymissing")) {
 			/* 회원MISSING정보업데이트-> 인계날짜 SYSDATE입력 */
